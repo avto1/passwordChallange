d26eddcc933e0b737a79d56d7ba85ab348ae5f96
avtandil zabakhidze <102874211+avtandilzabakhidze@users.noreply.github.com>
2023-06-18 21:36:31 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+string password = Console.ReadLine();
+
+Console.WriteLine(Result(password));
+
+string Result(string password)
+{
+    bool hasUpperCase = Regex.IsMatch(password, "[A -Z]");
+    bool hasLowerCase = Regex.IsMatch(password, "[a - z]");
+    bool hasNumber = Regex.IsMatch(password, "[0*]");
+    if(hasUpperCase && hasLowerCase && hasNumber)
+    {
+        return password;
+    }
+    else { password = Console.ReadLine(); }
+    return "";
+}
+
